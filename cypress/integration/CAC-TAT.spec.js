684cee669976a605b8549134974acdb1935d3825
unknown <cadumaster03@gmai.com>
2023-08-25 18:20:05 -0300

    continuacao aula 2

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,55 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function () {
-    it('verifica o título da aplicação', function () {
+    beforeEach(() => {
         cy.visit('./src/index.html')
+      })
+    
+    it('verifica o título da aplicação', function () {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+    it('clica e digita em campos obrigatórios', function () {
+        cy.get('#firstName')
+          .click()
+          .type('XPTO')
+
+        cy.get('#lastName')
+          .click()
+          .type('Xavier')
+
+        cy.get('#email')
+          .click()
+          .type('xptoxavier@gmail.com')
+        
+        cy.get('#open-text-area')
+          .click()
+          .type('Teste de escrita do Cypress')
+        
+        cy.get('.button')
+          .click()
+        
+        cy.get('.success').should('be.visible')
+    })
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
+        cy.get('#firstName')
+        .click()
+        .type('XPTO')
+
+      cy.get('#lastName')
+        .click()
+        .type('Xavier')
+
+      cy.get('#email')
+        .click()
+        .type('xptoxavier')
+      
+      cy.get('#open-text-area')
+        .click()
+        .type('Teste de escrita do Cypress')
+        cy.get('.button')
+          .click()
+
+        cy.get('.error')
+          .should('be.visible')
     })
 })
