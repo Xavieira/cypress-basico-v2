6289882aafe5285ff3b93bc3bdfc4e13b1599c60
unknown <cadumaster03@gmail.com>
2023-08-28 22:38:23 -0300

    correcao aula 3

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -137,7 +137,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
       .select('mentoria')
       .should('have.value', 'mentoria')
   })
-  it.only('seleciona um produto (Blog) por seu índice', function() {
+  it('seleciona um produto (Blog) por seu índice', function() {
     cy.get('#product')
       .select(1)
       .should('have.value', 'blog')
