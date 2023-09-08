a29a3cc997fb0388a93dec4ddc977aee86894119
unknown <cadumaster03@gmail.com>
2023-09-08 00:39:07 -0300

    aula 11

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -191,6 +191,29 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         expect($input[0].files[0].name).to.equal('example.json')
       })
   })
+  it('exibe e esconde as mensagens de sucesso e erro usando o .invoke()', function() {
+    cy.get('.success')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Mensagem enviada com sucesso.')
+      .invoke('hide')
+      .should('not.be.visible')
+
+    cy.get('.error')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Valide os campos obrigatórios!')
+      .invoke('hide')
+      .should('not.be.visible')
+  })
+  it('preenche a area de texto usando o comando invoke', function() {
+    const text = Cypress._.repeat('XPTO ', 15)
+    cy.get('#open-text-area')
+      .invoke('val', text)
+      .should('have.value', text)
+  })
   it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
     cy.get('#privacy a')
       .should('have.attr', 'target', '_blank')
@@ -199,5 +222,14 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     cy.get('#privacy a')
       .invoke('removeAttr', 'target', '_blank')
       .click()
+  })
+  it('faz uma requisição HTTP', function() {
+    cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+      .should(function(response) {
+        const { status, statusText, body } = response
+        expect(status).to.equal(200)
+        expect(statusText).to.equal('OK')
+        expect(body).include('CAC TAT')
+      })
   })
 })
